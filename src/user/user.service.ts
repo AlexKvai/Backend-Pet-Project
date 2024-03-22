666070263d834f461784c3212c0a18fc480ffd76
alex parfenov <alexp-2004@mail.ru>
2024-03-22 18:07:38 +0700

    Добавлен метод getByEmail в user.service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { hash } from 'argon2';
+import { RegDto } from 'src/auth/dto/auth.dto';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
@@ -9,5 +11,21 @@ export class UserService {
     return this.prisma.user.findUnique({
       where: { id },
     });
+  }
+
+  getByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+  async create(dto: RegDto) {
+    const user = {
+      email: dto.email,
+      name: dto.name,
+      password: await hash(dto.password),
+    };
+    //@ts-ignore
+    return this.prisma.user.create({ data: user });
   }
 }
