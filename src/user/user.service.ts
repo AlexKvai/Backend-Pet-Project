c2c42a525a0bab5c2f5546937561af114ab65a49
alex parfenov <alexp-2004@mail.ru>
2024-03-23 12:30:02 +0700

    Добавление обновления профиля

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { hash } from 'argon2';
 import { RegDto } from 'src/auth/dto/auth.dto';
 import { PrismaService } from 'src/prisma.service';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
 export class UserService {
@@ -19,13 +20,35 @@ export class UserService {
     });
   }
 
+  async getProfile(id: string) {
+    const profile = await this.getById(id);
+    return profile;
+  }
+
   async create(dto: RegDto) {
     const user = {
       email: dto.email,
       name: dto.name,
       password: await hash(dto.password),
     };
-    //@ts-ignore
     return this.prisma.user.create({ data: user });
+  }
+
+  async update(id: string, dto: UserDto) {
+    let data = dto;
+    if (dto.password) {
+      data = { ...dto, password: await hash(dto.password) };
+    }
+    return this.prisma.user.update({
+      where: {
+        id,
+      },
+      data,
+      select: {
+        email: true,
+        name: true,
+        role: true,
+      },
+    });
   }
 }
